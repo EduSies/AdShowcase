7e0dbd6c73bef8611e9447a3cb428c5c96dba2be
Edu Sierra <edusies89@gmail.com>
2026-01-04 12:20:36 +0100

    chore: debugger yii

--- config/main-dev.php
+++ config/main-dev.php
@@ -1,11 +1,13 @@
 <?php
 
 // configuration adjustments for 'dev' environment
-$config['bootstrap'][] = 'debug';
-$config['modules']['debug'] = [
-    'class' => 'yii\debug\Module',
-    'allowedIPs' => ['*'],
-];
+if (YII_DEBUG) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
+    ];
+}
 
 $config['bootstrap'][] = 'gii';
 $config['modules']['gii'] = [
